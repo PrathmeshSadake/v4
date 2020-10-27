4d018b3bf7ba7cd8cbb7a4334539685defc55442
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-10-27 14:37:43 +0530

    Created task model to interact with database

---
+++ lib/models/task_model.dart
@@ -0,0 +1,33 @@
+class Task {
+  int id;
+  String title;
+  String priority;
+  DateTime date;
+  int status; //0 - imcomplete, 1- complete
+
+  Task({this.title, this.date, this.priority, this.status});
+  Task.withId({this.title, this.date, this.priority, this.id, this.status});
+
+  Map<String, dynamic> toMap() {
+    final map = Map<String, dynamic>();
+    if (id != null) {
+      map['id'] = id;
+    }
+    map['title'] = title;
+    map['date'] = date;
+    map['priority'] = priority;
+    map['status'] = status;
+
+    return map;
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task.withId(
+      id: map['id'],
+      title: map['title'],
+      priority: map['priority'],
+      date: map['date'],
+      status: map['status'],
+    );
+  }
+}
